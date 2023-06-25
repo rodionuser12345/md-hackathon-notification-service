876460850f11a8180e92ea8f5a043146bba3f8e3
rodio <busurmanov.rodion@gmail.com>
2023-06-25 11:44:38 +0300

    Init.

---
+++ src/test/java/md/hackathon/springboot/notification_service_app/NotificationServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package md.hackathon.springboot.notification_service_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
